6f9157e7b66bbc2ad2a6cc44ec847e3ebacf744d
QianRui <1135901259@qq.com>
2020-06-09 13:53:28 +0800

    1.时间工具类加强

--- src/main/java/common/util/DateUtil.java
+++ src/main/java/common/util/DateUtil.java
@@ -1,5 +1,6 @@
 package common.util;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
@@ -10,22 +11,30 @@ import java.util.concurrent.ConcurrentHashMap;
  * 2018/5/31
  */
 public class DateUtil {
-	
-	private static final Map<String, ThreadLocal<SimpleDateFormat>> SIMPLE_DATE_FORMAT_FACTORY = new ConcurrentHashMap<>();
-	
-	private static ThreadLocal<SimpleDateFormat> getSimpleDateFormat(String format) {
-		return SIMPLE_DATE_FORMAT_FACTORY.computeIfAbsent(format,
-				a -> ThreadLocal.withInitial(() -> new SimpleDateFormat(a)));
-	}
-	
-	private static String format(String format, Date date) {
-		return date == null ? null : getSimpleDateFormat(format).get()
-				.format(date);
-	}
-	
 
-	public static String format(String format, long date) {
-		return format(format, new Date(date));
-	}
+    private static final Map<String, ThreadLocal<SimpleDateFormat>> SIMPLE_DATE_FORMAT_FACTORY = new ConcurrentHashMap<>();
+
+    private static ThreadLocal<SimpleDateFormat> getSimpleDateFormat(String format) {
+        return SIMPLE_DATE_FORMAT_FACTORY.computeIfAbsent(format,
+                a -> ThreadLocal.withInitial(() -> new SimpleDateFormat(a)));
+    }
+
+    public static String format(String format, Date date) {
+        return date == null ? null : getSimpleDateFormat(format).get()
+                .format(date);
+    }
+
+    public static Date parse(String format, String str) {
+        try {
+            return str == null ? null : getSimpleDateFormat(format).get()
+                    .parse(str);
+        } catch (ParseException e) {
+            return ExceptionUtil.throwT(e);
+        }
+    }
+
+    public static String format(String format, long date) {
+        return format(format, new Date(date));
+    }
 
 }
